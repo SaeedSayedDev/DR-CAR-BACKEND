0be24af04b27fd6a43b72649d39046eb50d08b2b
abdoSalahWork <abdosalahwork@gmail.com>
2024-02-03 17:01:10 +0200

    handel data seeder

--- database/seeders/HandelDataSeeder.php
+++ database/seeders/HandelDataSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\BookingService;
+use App\Models\GarageInformation;
 use App\Models\OrderStatus;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Artisan;
@@ -17,42 +18,13 @@ class HandelDataSeeder extends Seeder
     public function run()
     {
         //
-        $data = [
-            [
-                "status" => "Received",
-            ],
-            [
-                "status" => "In Progress",
-            ],
-            [
-                "status" => "On the Way",
-
-            ],
-            [
-                "status" => "Accepted",
-
-            ],
-            [
-                "status" => "Ready",
-
-            ],
-            [
-                "status" => "Done",
-
-            ],
-            [
-                "status" => "Failed",
-
-            ]
+        $garage_informations = [
+            ['phone_number' => '01096505007', 'address' => 'address_3', 'short_biography' => 'short_biography_3', 'phone_verified_at' => '2023-11-17 14:30:43', 'garage_id' => 5],
+            ['phone_number' => '01096505006', 'address' => 'address_4', 'short_biography' => 'short_biography_4', 'phone_verified_at' => '2023-11-17 14:30:43', 'garage_id' => 6],
+            ['phone_number' => '01096505005', 'address' => 'address_5', 'short_biography' => 'short_biography_5', 'phone_verified_at' => '2023-11-17 14:30:43', 'garage_id' => 7]
         ];
-        OrderStatus::insert($data);
-        $bookingServicesOld = BookingService::get();
-        Artisan::call('migrate');
-
-        $bookingServices = BookingService::get();
-
-        for ($i = 0; $i < count($bookingServices); $i++) {
-            $bookingServices[$i]->update(['order_status_id' => $bookingServicesOld[$i]->order_status_id]);
+        foreach ($garage_informations as $garage_information) {
+            GarageInformation::create($garage_information);
         }
     }
 }
