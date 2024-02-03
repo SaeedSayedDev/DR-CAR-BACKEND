451e2047e1f7412cf391faa6b7f9bc416a43fdfd
saeed <saidsayed9061@gmail.com>
2024-02-03 19:30:24 +0200

    notification message , delete notitfication

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\MessageNotificationRequest;
 use App\Models\Notification;
+use App\Services\NotificationService;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
-    //
+    public function __construct(private NotificationService $notificationService)
+    {
+    }
 
     public function index()
     {
@@ -44,6 +48,36 @@ class NotificationController extends Controller
             'data' => $notification,
             "message" => "Notifications retrieved successfully"
 
+        ]);
+    }
+    public function delete($notification_id)
+    {
+        $user = auth()->user();
+        Notification::where('user_id', $user->id)->findOrFail($notification_id)->delete();
+        return response()->json([
+            'success' => true,
+            "message" => "Notification deleted successfully"
+
+        ]);
+    }
+
+    public function messageNotification(MessageNotificationRequest $request)
+    {
+        $user = auth()->user();
+        $type_id = 0;
+        $creator_name = $user->full_name;
+        $reciver_id = $request->user_id;
+        $notification_type_en = 'message';
+        $notification_type_ar = 'رسالة';
+        $text_en = $user->full_name . 'sent a new message';
+        $text_ar = $user->full_name . 'ارسل لك رسالة جديدة ';
+        $api = 'message api';
+
+
+        $this->notificationService->notification($type_id, $creator_name,  $text_en, $text_ar, $notification_type_en, $notification_type_ar, $api, $reciver_id);
+        return response()->json([
+            'success' => true,
+            "message" => "notification send successfully"
         ]);
     }
 }
