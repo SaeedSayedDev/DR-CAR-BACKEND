da595759f5a5a106633964d40785cbdd0846b380
saeed <saidsayed9061@gmail.com>
2024-02-04 00:15:45 +0200

    ->onDelete('cascade')

--- database/migrations/2023_11_25_181908_create_reviews_table.php
+++ database/migrations/2023_11_25_181908_create_reviews_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->foreign('service_id')->references('id')->on('services')->onDelete('cascade');
 
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
