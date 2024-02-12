bf9b6cca9f5b9b80a613b5d65d29c1150b395a11
abdoSalahWork <abdosalahwork@gmail.com>
2024-02-12 14:55:49 +0200

    get notificaqtion found

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Admin\Service;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,4 +12,16 @@ class Notification extends Model
     protected $fillable = [
         'type_id', 'creator_name', 'user_id', 'text_en', 'text_ar', 'api', 'notification_type_en', 'notification_type_ar', 'read'
     ];
+
+    function booking_service()
+    {
+        return $this->belongsTo(BookingService::class, 'type_id')
+            ->where('api', 'LIKE', '%api/booking/show%');
+    }
+
+    function booking_winch()
+    {
+        return $this->belongsTo(BookingWinch::class, 'type_id')
+            ->where('api', 'LIKE', '%api/booking/winch/show%');
+    }
 }
